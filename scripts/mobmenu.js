41032027441d3e1d8826317c831823371eeff71a
RaevschiCatalin <raevschicatalin@gmail.com>
2023-08-25 09:55:16 +0300

    mob nav on main pages

--- scripts/mobmenu.js
+++ scripts/mobmenu.js
@@ -1,7 +1,6 @@
 const mobileMenuButton = document.getElementById("mobile_menu_button");
 const navMenu = document.querySelector("nav ul");
-const navIcons = document.querySelector("nav .mobile_nav");
+
 mobileMenuButton.addEventListener("click", () => {
     navMenu.classList.toggle("active");
-    navIcons.classList.toggle("active");
 });
